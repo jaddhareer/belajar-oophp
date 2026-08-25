6f8a7c956b39a796289b88cf7ee2960f998d0b2a
jaddhareer <jaddhareer@gmail.com>
2026-08-25 20:48:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Belajar OOP";
+echo "<br>";
+echo "di project ini, saya akan mempelajari OOP PHP agar bisa memahami konsep programming menggunakan metode OOP. targetnya adalah nanti saya bisa membuat aplikasi CRUD sederhana ke database. bentuknya adalah aplikasi pencatatan stok gudang yang memiliki sistem login. jadi nanti akan ada 3 tabel, tabel stok, tabel transaksi in dan out, dan user.";
